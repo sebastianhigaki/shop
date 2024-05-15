22fe4631024adba41c27c97d068538329da03a03
“sebastianhigaki” <shr_sebas02@hotmail.com>
2024-05-14 20:09:45 -0500

    details display renderace

--- scripts/productCards.js
+++ scripts/productCards.js
@@ -1,54 +1,60 @@
 const products = [
   {
     id: 1,
-    image: "assets/mock1.jpg",
+    image: ["assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg"], 
     title: "Macbook Pro 15'4",
-    description: "Black",
+    colors: ["Black", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 3399,
     discount: 15,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
   },
   {
     id: 2,
-    image: "assets/mock2.jpg",
+    image: ["assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg"],
     title: "Macbook Air 13'3",
-    description: "Space Gray",
+    colors: ["Space Gray", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 1500,
     discount: 5,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
   },
   {
     id: 3,
-    image: "assets/mock1.jpg",
+    image: ["assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg"],
     title: "Macbook Pro 16'0",
-    description: "White",
+    colors: ["Black", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 4200,
     discount: 50,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
   },
   {
     id: 4,
-    image: "assets/mock2.jpg",
+    image: ["assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg"],
     title: "Macbook Air 13'3",
-    description: "Space Black",
+    colors: ["Space Gray", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 3500,
     discount: 50,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
   },
   {
     id: 5,
-    image: "assets/mock1.jpg",
+    image: ["assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg"],
     title: "Iphone 12",
-    description: "Gray",
+    colors: ["Black", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 900,
     discount: 10,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
   },
   {
     id: 6,
-    image: "assets/mock2.jpg",
+    image: ["assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg", "assets/mock1.jpg", "assets/mock2.jpg"],
     title: "Macbook Air 12'9",
-    description: "Gold",
+    colors: ["Space Gray", "Silver", "Gold"],
+    description: "Experimenta el rendimiento revolucionario del chip M1 de Apple en la MacBook Pro 13, con una pantalla Retina de 13 pulgadas y colores vibrantes. Equipada con opciones de almacenamiento SSD ultrarrápidas y hasta 16 GB de memoria unificada, ofrece potencia y eficiencia para tus proyectos más exigentes. Además, cuenta con Thunderbolt 3 para una conectividad rápida y versátil.",
     price: 5000,
     discount: 30,
     taxPolicy: "Incluye impuesto País y percepción AFIP",
@@ -59,13 +65,16 @@ const products = [
 const productsSelector = document.getElementById("products");
 
 function createCard(product) {
+  let imageSrc;
+  if (Array.isArray(product.image)) {
+    imageSrc = product.image[0];
+  }
   return `
     <article class="product-card">
       <a class="product-card" href="./details.html?id=${product.id}">
-          <img class="product-img" src="${product.image}" alt="${product.title}">
+          <img class="product-img" src="${imageSrc}" alt="${product.title}">
           <div class="product-info">
             <span class="product-title">${product.title}</span>
-            <span class="product-description">${product.description}</span>
             <div class="product-price-block">
               <span class="price">$${product.price}</span>
               <span class="discount">${product.discount}% Off</span>
--- scripts/productDetail.js
+++ scripts/productDetail.js
@@ -1,7 +1,87 @@
-
-const params = new URLSearchParams(query);
 const query = location.search;
+const params = new URLSearchParams(query);
 const id = params.get("id");
 console.log(id);
 
+function printDetails(products, id) {
+  const product = products.find((product) => product.id == id);
+  const detailsTemplate = `
+      <div id="Detail" class="columns-container">
+        <div class="product-images-block">
+          ${product.image
+            .map(
+              (img, index) => `
+                <div class="thumbnail-container">
+                  <img class="miniature" src="${img}" alt="${
+                product.title
+              } imagen ${index + 1}" />
+                </div>
+              `
+            )
+            .join("")}
+          <img class="main-image" src="${product.image[0]}" alt="${
+    product.title
+  }" />
+        </div>
+        <div class="product-description-block">
+          <h1 class="title">${product.title}</h1>
+          <form class="selector">
+            <fieldset>
+              <label class="label" for="color">Color</label>
+              <select type="text" placeholder="Selecciona un color">
+                ${product.colors
+                  .map((color) => `<option value="${color}">${color}</option>`)
+                  .join("")}
+              </select>
+            </fieldset>
+          </form>
+          <div class="description">
+            <h1>Descripcion:</h1>
+            <p>${product.description}</p>
+          </div>
+        </div>
+        <div class="product-checkout-block">
+          <span class="checkout-total-label">Total:</span>
+          <h2 class="checkout-total-price">$${product.price}</h2>
+          <!-- Aquí van las políticas de envío y proceso de compra -->
+          <p class="checkout-description">
+            Incluye impuesto PAIS y percepción AFIP. Podés recuperar AR$ 50711 haciendo la solicitud en AFIP.
+          </p>
+          <ul class="checkout-policy-list">
+            <li>
+              <span class="policy-icon"><img src="../assets/truck.png" alt="Truck"/></span>
+              <span class="policy-desc">Agrega el producto al carrito para conocer los costos de envío</span>
+            </li>
+            <li>
+              <span class="policy-icon"><img src="../assets/plane.png" alt="Plane"/></span>
+              <span class="policy-desc">Recibe aproximadamente entre 5 y 10 días hábiles, seleccionando envío normal</span>
+            </li>
+          </ul>
+          <div class="checkout-process">
+            <div class="top">
+              <input type="number" value="1" />
+              <button class="btn-primary">Comprar</button>
+            </div>
+            <div class="bottom">
+              <button class="btn-outline">Añadir al Carrito</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
 
+  const detailsSelector = document.querySelector("#details");
+  detailsSelector.innerHTML = detailsTemplate;
+
+  function handleMiniatureClick(event) {
+    const clickedImageSrc = event.target.getAttribute("src");
+    const mainImage = document.querySelector(".main-image");
+    mainImage.setAttribute("src", clickedImageSrc);
+  }
+  const miniatureImages = document.querySelectorAll(".miniature");
+  miniatureImages.forEach((image) => {
+    image.addEventListener("click", handleMiniatureClick);
+  });
+}
+
+printDetails(products, id);
